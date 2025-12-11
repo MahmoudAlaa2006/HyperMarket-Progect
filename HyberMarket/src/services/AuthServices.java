a68931a8ce3e52e7c6b968faf53df0b6969d4fc9
M-Elshamy3 <mazenashraf2666@gmail.com>
2025-12-11 18:21:05 +0200

    Refactor AuthServices to manage user authentication
    Refactored AuthServices to include EmployeeRepository and userList initialization. Updated login method to save userList after login.

--- HyberMarket/src/services/AuthServices.java
+++ HyberMarket/src/services/AuthServices.java
@@ -1,13 +1,28 @@
 package services;
-
+import data.EmployeeRepository;
 import models.User;
-
+import java.time.LocalDateTime;
+import java.util.List;
 public class AuthServices {
-    public User login(String email, String password){
-        /* return user object with role info */
+    private EmployeeRepository repo;
+    private List<User> userList;
+    public AuthServices() {
+        this.repo = new EmployeeRepository();
+        this.userList = repo.load();        
+        System.out.println("System loaded " + userList.size() + " users from file.");
     }
-
-    public void logout(User user){
-
+    public User login(String email, String password) {
+        for (User u : userList) {
+            if (u.getEmail().equalsIgnoreCase(email) && u.getPassword().equals(password)){u.setLastLogin(LocalDateTime.now());
+                repo.save(userList); 
+                return u;
+            }
+        }
+        return null;
+    }
+    public void logout(User user) {
+        if(user != null) {
+            System.out.println("User logged out: " + user.getName());
+        }
     }
 }
