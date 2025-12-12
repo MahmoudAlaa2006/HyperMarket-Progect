1cc93f11f46ce7320cffda1b5e0659fc62e462b6
medo123-comment <mohamed.sayedfk2020@gmail.com>
2025-12-12 23:50:21 +0200

    Update UserServices.java

--- HyberMarket/src/services/UserServices.java
+++ HyberMarket/src/services/UserServices.java
@@ -6,50 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import models.User;
-
 public class UserServices {
-    //-----------------------update user info----------------
-    public void updateUserInfo(User user) {
-    File f = new File("C:\\Users\\hp\\Desktop\\user services\\user.txt");
-    if (!f.exists()) {
-        System.out.println("File doesn't exist");
-        return;
-    }
-
-    List<String> lines = new ArrayList<>();
-
-    try (Scanner input = new Scanner(f)) {
-        while (input.hasNextLine()) {
-            String line = input.nextLine();
-
-            // لو السطر فيه نفس الـ ID
-            if (line.contains("userID=\"" + user.getId() + "\"") || line.contains("id=\"" + user.getId() + "\"")) {
-
-                // نحدث باقي البيانات
-                line = line.replaceAll("name=\"[^\"]*\"", "name=\"" + user.getName() + "\"");
-                line = line.replaceAll("email=\"[^\"]*\"", "email=\"" + user.getEmail() + "\"");
-                line = line.replaceAll("password=\"[^\"]*\"", "password=\"" + user.getPassword() + "\"");
-                line = line.replaceAll("role=\"[^\"]*\"", "role=\"" + user.getRole() + "\"");
-            }
-
-            lines.add(line);
-        }
-    } catch (Exception e) {
-        e.printStackTrace();
-        return;
-    }
-
-    // كتابة الملف بعد التعديل
-    try (PrintWriter writer = new PrintWriter(f)) {
-        for (String l : lines) {
-            writer.println(l);
-        }
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
-//---------------------change username----------------
+    //---------------------change username----------------
     public boolean changeUsername(String userID, String newName){
         File f = new File("C:\\Users\\hp\\Desktop\\user services\\user.txt");
 
